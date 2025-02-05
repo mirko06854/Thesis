5f7db605b0840e9f8d81724efa90e55e07e4d8d9
Mirko Bandello <mirko0313@gmail.com>
2025-02-05 15:49:00 +0100

    cleaned code

--- section 3/LLM_with_llama/LLM_GDPR_llama.py
+++ section 3/LLM_with_llama/LLM_GDPR_llama.py
@@ -4,27 +4,6 @@ from groq import Groq
 import os
 import datetime
 
-# Funzione per estrarre i nomi delle attività da un file BPMN
-def parse_bpmn_for_activities(bpmn_file):
-    tree = ET.parse(bpmn_file)
-    root = tree.getroot()
-    activities_without_consent = []
-
-    # Regex per estrarre il nome dell'attività dal formato "<bpmn:task id="Task_..." name="Task Name">"
-    name_regex = re.compile(r'name="([^"]+)"')
-
-    # Traverso il file BPMN per trovare i task
-    for activity in root.findall('.//bpmn:task', namespaces={'bpmn': 'http://www.omg.org/spec/BPMN/20100524/MODEL'}):
-        # Estrazione del nome dell'attività con la regex
-        name_match = name_regex.search(ET.tostring(activity).decode())
-        if name_match:
-            # Verifica se l'attività ha il consenso (presenza dell'attributo "consent")
-            consent = activity.attrib.get('consent', '').lower()
-            if consent == 'no':  # Se l'attività non ha consenso
-                activities_without_consent.append(name_match.group(1))  # Aggiungi il nome dell'attività
-
-    return activities_without_consent
-
 # Funzione per pulire la risposta dell'LLM mantenendo "numero. yes" o "numero. no"
 def clean_llm_response(response):
    # debug -> print("Original LLM Response:")  # Stampa la risposta originale
@@ -45,7 +24,7 @@ def clean_llm_response(response):
 # Funzione principale per leggere il file di descrizione del lavoro e generare le risposte
 def generate_answers_and_responses():
     # Inizializza il client Groq
-    client = Groq(api_key='PUT_HERE_YOUR_API_KEY')
+    client = Groq(api_key='PUT_YOUR_API_KEY_HERE')
 
     # Leggi il file di descrizione del lavoro (contiene le domande)
     with open('jobDescription.txt', 'r') as job_description_file:
@@ -76,8 +55,6 @@ def generate_answers_and_responses():
     # Separare le risposte
     answers = cleaned_answers.split('\n')
 
-    # Carica le attività dal file BPMN
-    activities_without_consent = parse_bpmn_for_activities(r'C:\Users\ferru\Desktop\parte2 tesi tests\self-service-restaurant.bpmn')
 
     # Scrivere le risposte in un file di output in formato Markdown
     timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
@@ -96,8 +73,7 @@ def generate_answers_and_responses():
             answer = answer.strip().lower()  # Normalizza la risposta
 
             output_file.write(f"## {question}\n")
-            output_file.write(f"{answer}\n\n")           
-
+            output_file.write(f"{answer}\n\n")
 
 # Esegui la funzione principale
 generate_answers_and_responses()
